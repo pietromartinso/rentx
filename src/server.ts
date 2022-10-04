978b576d605b32a0ecd060d24bede6521734982c
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-10-04 10:35:10 -0300

    DER + Adding Categories routes code: post new category

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
-});
-
-app.post("/courses", (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running on localhost:3333"));
