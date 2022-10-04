07ec11a683bb0273f5fb4ffbb1ccc9d689221475
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-10-04 11:49:13 -0300

    Added Category Repository and ceateDTO

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,15 @@
 import { Router } from "express";
 
+import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CatetoriesRepository";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
