71fd8f9ea9c90191999ecf542b15adcc9496c828
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-10-04 09:57:29 -0300

    server.ts tests and debug

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
+});
+
+app.listen(3333, () => console.log("Server is running on localhost:3333"));
