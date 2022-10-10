126ae3b53caa08b9912c043d79183d9c67bb632f
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-10-10 15:27:45 -0300

    Added user migrations, creation, simple validaton and password encryption

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcryptjs";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExsits = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExsits) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
