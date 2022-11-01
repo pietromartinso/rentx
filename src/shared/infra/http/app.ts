633359cd4c6f65484a9f4ff1e7f2c05da916e597
Pietro Martins de Oliveira <profpietromartins@gmail.com>
2022-11-01 12:06:59 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
@@ -21,6 +22,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
